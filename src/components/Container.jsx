dc815145851ec9353ad14845a3b5960522edfce0
Andre Silva <andre.2107.silva@gmail.com>
2017-11-16 02:30:07 +0000

    Some boiler plater done (grid, palette)

---
+++ src/components/Container.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Square from './Square';
+
+class Container extends React.Component{
+  renderSquares() {
+    let numSquares = [];
+    const rows = 20;
+
+    for (let i = 0; i < (rows); i++) {
+      numSquares.push(i);
+    }
+
+    const ret = numSquares.map(item => {
+      return numSquares.map(elem => {
+        return <Square key={item + elem} row={item} column={elem} />;
+      })
+    });
+
+    return ret;
+  }
+  
+  render() {
+    return (
+    <div style={ styles }>
+      {this.renderSquares()}
+    </div>
+    );
+  }
+};
+
+const styles = {
+  width: '600px',
+  height: '600px',
+  border: '1px solid #ccc',
+  display: 'inline-block',
+};
+
+export default Container;
